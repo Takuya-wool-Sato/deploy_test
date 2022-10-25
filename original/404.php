db735c4ababb5c1247f4b9e08243298d46718cd2
Takuya-wool-Sato <takuya@hab-co.jp>
2022-10-25 13:56:20 +0900

    入れ替え

---
+++ original/404.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+<div class="container page-second page-404">
+  <section class="section-group">
+    <div class="section-group__inner">
+    </div>
+  </section>
+</div>
+<?php get_footer(); ?>
