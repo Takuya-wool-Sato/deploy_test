0fe51639313540299c8f1174fec09b863d5b89dd
Takuya-wool-Sato <takuya@hab-co.jp>
2022-10-25 13:44:26 +0900

    ファーストコミット

---
+++ original/index.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+<div class="container top-page">
+  <section class="section-group main">
+  </section>
+  <section class="section-group">
+    <div class="section-group__inner">
+    </div>
+  </section>
+</div>
+<?php get_footer(); ?>
---
+++ original/page-contact.php
@@ -0,0 +1,11 @@
+<?php get_header(); ?>
+<div class="container page-second page-contact">
+  <div class="second-header">
+    <img src="<?php bloginfo('template_directory'); ?>/images/second-header_logo.svg" alt="top_logo" class="top-logo pc" width="">
+  </div>
+  <section class="section-group">
+    <div class="section-group__inner">
+    </div>
+  </section>
+</div>
+<?php get_footer(); ?>
